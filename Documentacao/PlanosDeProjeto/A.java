b04b309a39a28df0523e745c416fef6f119902c5
DouglasGeovanne01 <60403869+DouglasGeovanne01@users.noreply.github.com>
2021-03-29 09:32:19 -0300

    Add files via upload

---
+++ Documentacao/PlanosDeProjeto/A.java
@@ -0,0 +1,17 @@
+package OrientacaoObj;
+
+import java.util.Scanner;
+
+public class A {
+
+    public static void main(String[] args) {
+    System.out.println();
+
+    Scanner ler = new Scanner(System.in);
+
+        final int x = 10;
+
+    ler.close();
+    }
+
+}
